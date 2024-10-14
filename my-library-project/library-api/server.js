edecfb0c68912cc175d80ab5dad2d4b330414467
RSVictor <victorribeiro304@hotmail.com>
2024-10-14 20:20:31 -0300

    Atualizado

--- my-library-project/library-api/server.js
+++ my-library-project/library-api/server.js
@@ -1,25 +1,39 @@
-const express = require('express');
-const mongoose =  require('mongoose');
+// arquivo principal da api
+// inicia o servidor
+const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser');
+// inicialização do app
 
-// Inicialização do app
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexão ao MongoDB
-mongoose.connect('mongodb+srv://Victor:victor123@cluster0.ngnna.mongodb.net/library', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB conectado'))
-.catch(err => console.log('Erro ao conectar ao MongoDB:', err));
+const authRoutes = require('./routes/authRoutes')
 
-//Importação das rotas
-const booksRoutes = require('./routes/books');
-app.use('/api/books', booksRoutes);
+app.use(cors ({
+    origin: 'http://localhost:8080', //Permite requisições apenas do seu froontend
+    methods: ['GET', 'POST'], //Adicione outros métodos se necessario
+}));
 
-// Definir a porta do servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
-})
+// Conexão com o mongo db
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser:true,useUnifiedTopology:true
+}
+   
+    
+    
+).then(()=>console.log('Mongodb conectado')).catch(err=>console.error('Erro ao conectar no mongo',err));
+
+const PORT = process.env.PORT || 5000; //Define a parte do servidor, usando variavel de ambiente ou padrão 5000
+
+// Importação das rotas
+const bookRoutes = require('./routes/books');
+app.use('/api/books',bookRoutes); // irá retornar a rota dos livros
+app.use('/api/auth',authRoutes);
+// Define a porta do servidor
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando na porta ${PORT}`); //Inicia o servidor e exibe a mensagem no console
+}); 
